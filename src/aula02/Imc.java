99122987654b4cdb054d13f58c2757de870345fa
celso.alunos <joaopedro.dancam@gmail.com>
2024-05-13 14:29:14 -0300

    'Atualização'

--- src/aula02/Imc.java
+++ src/aula02/Imc.java
@@ -1,32 +1,44 @@
 package aula02;
 
+// Importando a biblioteca Scanner
 import java.util.Scanner;
 
 public class Imc {
     public static void main(String[] args) {
+
+        //Criando o objeto scanner
         Scanner leitor = new Scanner(System.in);
 
+        //Pegando dados e criando variaveis
         System.out.print("Digite seu peso: ");
         short peso = leitor.nextShort();
 
         System.out.print("Digite a sua altura: ");
         double altura = leitor.nextDouble();
 
+        String classificacao = "";
+
+        //Calculando o IMC
         double imc = peso/(altura*altura);
 
-        System.out.println(imc);
+
+        //Decidinco o IMC do usuario
         if (imc < 18.5) {
-            System.out.println("Você esta abaixo do peso ideal");
+            classificacao = "Peso ideal";
         } else if (imc > 18.5 && imc <= 25) {
-            System.out.printf("Você esta no peso ideal ");
+            classificacao = "Peso ideal ";
         } else if (imc > 25.0 && imc <= 30) {
-            System.out.println("Você esta levemente acima do peso");
+            classificacao = "Levemente acima do peso";
         } else if (imc > 30 && imc <=35) {
-            System.out.println("Você esta com obesidade grau 1");
-        } else if (imc > 35 && imc <=40) {
-            System.out.println("Você esta com obesidade grau 2");
+            classificacao = "Obesidade grau 1";
+        } else if (imc > 35 && imc <=40){
+            classificacao = "Obesidade grau 2";
         } else if (imc > 40) {
-            System.out.println("Você esta com obesidade grau 3");
+            classificacao = "Obesidade grau 3";
         }
+
+        //Monstrado o resultado
+        String mensagem = String.format("O resultado do IMC foi %.2f. A sua classificação é %s", imc, classificacao);
+        System.out.println(mensagem);
     }
 }
